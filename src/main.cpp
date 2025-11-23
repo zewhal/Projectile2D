eed449310409f38c2595c0d6aaaca84a968c3b8b
zewhal <mjabelcina.dev@gmail.com>
2025-11-24 04:16:09 +0800

    Added position update and draw 2D circle

--- src/main.cpp
+++ src/main.cpp
@@ -14,18 +14,41 @@ struct Ball {
           velX(vx),
           velY(vy),
           radius(r) {}
+
+    void UpdatePosition(float kGravity, float deltaTime){
+        // TODO: Add collision to wall
+        velY += kGravity * deltaTime;
+        posX += velX * deltaTime;
+        posY += velY * deltaTime;
+    }
+
+    void Draw(){
+        DrawCircleV({posX, posY}, radius, RED);
+    }
 };
 
 
 int main() {
     const int kWidth = 750;
     const int kHeight = 750;
+    const float kGravity = 50.0f;
     InitWindow(kWidth, kHeight, "Projectile2D");
     SetTargetFPS(60);
 
+    Ball ball{
+        50,
+        kHeight - 50,
+        200,
+        -200,
+        50
+    };
     while (!WindowShouldClose()) {
+        float deltaTime = GetFrameTime();
+        ball.UpdatePosition(kGravity, deltaTime);
+        
         BeginDrawing();
         ClearBackground(BLACK);
+        ball.Draw();
         EndDrawing();
     }
 
