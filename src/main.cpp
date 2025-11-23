335ccdae2e0d10c54c3d078429ef8e9bd49e19a0
zewhal <mjabelcina.dev@gmail.com>
2025-11-24 03:53:59 +0800

    Add Ball struct with position, velocity, and radius

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 // COPYRIGHT (c) 2025 Zewhal
 #include <raylib.h>
 
+struct Ball {
+    float posX;
+    float posY;
+    float velX;
+    float velY;
+    float radius;
+
+    Ball(float x, float y, float vx, float vy, float r)
+        : posX(x),
+          posY(y),
+          velX(vx),
+          velY(vy),
+          radius(r) {}
+};
 
 
 int main() {
